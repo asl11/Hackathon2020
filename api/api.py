17116f1db067011840bcdfc96b95c1397fb3fdb9
asl11 <asl11@rice.edu>
2020-09-19 12:02:12 -0700

    website progress

--- api/api.py
+++ api/api.py
@@ -1,8 +1,39 @@
-import time
-from flask import Flask
+import json
+import os
+from flask import Flask, request, session
+from werkzeug.utils import secure_filename
+import logging
+import flask_cors
+
+logging.basicConfig(level=logging.INFO)
+
+logger = logging.getLogger('HELLO WORLD')
+
+
+
+UPLOAD_FOLDER = './uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
+@app.route('/upload', methods=['POST'])
+def fileUpload():
+    target=os.path.join(UPLOAD_FOLDER,'test_docs')
+    if not os.path.isdir(target):
+        os.mkdir(target)
+    logger.info("welcome to upload`")
+    file = request.files['file'] 
+    filename = secure_filename(file.filename)
+    destination="/".join([target, filename])
+    file.save(destination)
+    session['uploadFilePath']=destination
+    response=json.dumps("Hello")
+    return response
+
+if __name__ == "__main__":
+    app.secret_key = os.urandom(24)
+    app.run(debug=True,host="0.0.0.0",use_reloader=False)
+
+flask_cors.CORS(app, expose_headers='Authorization')
